160bc52ae0f00d6faa7d11d4bbde17fb8a971425
Michael Seidman <mseidman@email.sc.edu>
2024-05-01 15:43:33 -0500

    Day 2 Ruby

---
+++ strings_1.rb
@@ -0,0 +1,27 @@
+puts 'I like' + 'apple pie'
+
+puts 'I like ' + 'apple pie'
+puts 'I like' + ' apple pie'
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts '12' + 12
+puts '2' * '5'
+
+puts 'Betty' + 12
+puts 'Fred' + 'John'
+
+puts 'You\'re Swell!'
+
+/* puts 'You're Swell!' */
+
+
+
