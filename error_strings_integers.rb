2599338b7f474f087b4a18368011b5d3810bf70d
Michael Seidman <mseidman@email.sc.edu>
2024-05-02 19:02:17 -0500

    Day 3 Ruby

---
+++ error_strings_integers.rb
@@ -0,0 +1,3 @@
+var1 = 2
+var2 = '5'
+puts var1 + var2
---
+++ favorite_number.rb
@@ -0,0 +1,7 @@
+puts "What is your favorite number?"
+favorite_number = gets.chomp
+
+puts "Have you considered "
+puts favorite_number.to_i + 1
+puts "as a bigger and better favorite number?"
+
