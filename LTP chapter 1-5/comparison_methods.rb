f5a19902f25c07c6c9a8ed994add11faba4cba85
Michael Seidman <mseidman@email.sc.edu>
2024-05-10 02:09:20 -0500

    Day 6 Ruby
    Reorganize based on progress.

---
+++ LTP chapter 1-5/comparison_methods.rb
@@ -0,0 +1,16 @@
+puts 1 > 2
+puts 2 > 1
+
+puts 5 >= 5
+puts 5 <= 4
+
+puts 1 == 1
+puts 2 != 1
+
+puts "cat" < "dog"
+
+puts "a giant woman" < "Watermelon Steven"
+puts "a giant woman".downcase < "Watermelon Steven".downcase
+
+puts 2 < 10
+puts "2" < "10"
