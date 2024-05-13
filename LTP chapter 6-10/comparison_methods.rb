9985c0dc8adf8d923ebf8fd81bd44710975c893d
penguinito <129811323+penguinito@users.noreply.github.com>
2024-05-13 16:03:17 -0500

    Day 7 Ruby

---
+++ LTP chapter 6-10/comparison_methods.rb
@@ -0,0 +1,22 @@
+puts 1 > 2
+puts 1 < 1
+puts ""
+
+puts 5 >= 5
+puts 5 <= 4
+puts ""
+
+puts 1 == 1
+puts 2 != 1
+puts ""
+
+puts "cat" < "dog"
+puts ""
+
+puts "a giant woman" < "Watermelon Steven"
+puts "a giant woman".downcase < "Watermelon Steven".downcase
+puts ""
+
+puts 2 < 10
+puts "2" < "10"
+puts ""
